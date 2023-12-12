1d8529f62085b0eab803788e359c5e2351cee302
jano1906 <janolszewski1906@gmail.com>
2023-12-12 19:38:17 +0100

    fix

--- solutions/spark_solution.py
+++ solutions/spark_solution.py
@@ -1,8 +1,6 @@
 import argparse
 from spark_doubling import spark_doubling
 from spark_linear import spark_linear
-import spark_doubling_old
-import spark_linear_old
 
 parser = argparse.ArgumentParser()
 parser.add_argument("algorithm", choices=["linear", "doubling"])
@@ -14,8 +12,6 @@ if __name__ == "__main__":
     assert args.output.endswith(".csv"), f"got input file {args.output}"
 
     if args.algorithm == "linear":
-#        spark_linear_old.spark_linear(args.input, args.output)
         spark_linear(args.input, args.output)
     elif args.algorithm == "doubling":
-        spark_doubling_old.spark_doubling(args.input, args.output)
-#        spark_doubling(args.input, args.output)
+        spark_doubling(args.input, args.output)
